0745ca01196600b612da6b4162c940637140a547
firthowsa <firthowsasheikh77@gmail.com>
2020-09-03 09:47:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mini_exercise1_firthowsa_abdikadir/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.mini_exercise1_firthowsa_abdikadir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
